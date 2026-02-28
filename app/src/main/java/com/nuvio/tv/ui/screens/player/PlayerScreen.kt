2c831e760767b6bb4d5aaaa2734b63aba1b7ea23
tapframe <85391825+tapframe@users.noreply.github.com>
2026-03-01 01:38:20 +0530

    pressing back now dismisses intro/outro buttons

--- app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerScreen.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerScreen.kt
@@ -146,6 +146,10 @@ fun PlayerScreen(
             } else {
                 viewModel.onEvent(PlayerEvent.OnDismissEpisodesPanel)
             }
+        } else if (uiState.activeSkipInterval != null && !uiState.skipIntervalDismissed) {
+            viewModel.onEvent(PlayerEvent.OnDismissSkipIntro)
+        } else if (uiState.showNextEpisodeCard && uiState.nextEpisode != null) {
+            viewModel.onEvent(PlayerEvent.OnDismissNextEpisodeCard)
         } else if (uiState.showControls) {
             // If controls are visible, hide them instead of going back
             viewModel.hideControls()
