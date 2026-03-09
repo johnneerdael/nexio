000750a25533f527f727fd79cdf44712019cc27d
John Neerdael <jneerdael@netskope.com>
2026-03-09 16:50:17 +0100

    Release version 0.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "com.nexio.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 31
-        versionName = "0.13"
+        versionCode = 32
+        versionName = "0.14"
 
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
         buildConfigField("String", "TRAKT_CLIENT_ID", "\"${localProperties.getProperty("TRAKT_CLIENT_ID", "")}\"")
