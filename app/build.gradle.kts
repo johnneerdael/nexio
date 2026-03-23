b96d63a428790b441877588ee9b8b9b0a5c95f65
John Neerdael <jneerdael@netskope.com>
2026-03-23 01:38:37 +0100

    Bump version to 0.28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "com.nexio.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 45
-        versionName = "0.27"
+        versionCode = 46
+        versionName = "0.28"
 
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
         buildConfigField("String", "REAL_DEBRID_CLIENT_ID", "\"${resolveProperty(devProperties, localProperties, "REAL_DEBRID_CLIENT_ID")}\"")
