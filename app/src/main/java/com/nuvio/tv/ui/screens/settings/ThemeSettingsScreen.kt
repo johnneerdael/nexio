754ce0f7cb9a4355e5507aecb4fbd17d206ff8f3
Ircas <50799513+Ircas@users.noreply.github.com>
2026-03-27 02:24:37 +0200

    Update ThemeSettingsScreen.kt

--- app/src/main/java/com/nuvio/tv/ui/screens/settings/ThemeSettingsScreen.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/settings/ThemeSettingsScreen.kt
@@ -95,7 +95,7 @@ fun ThemeSettingsContent(
         val tags = listOf(
             "en", "de", "es", "es-419", "hu", "fr", "it", "pl",
             "pt-PT", "pt-BR", "tr", "cs", "sk", "sl", "sv", "ro", "ja",
-            "nl", "vi", "hi", "lt"
+            "nl", "vi", "hi", "lt", "el" 
         )
         listOf(null to strLanguageSystem) + tags.map { tag ->
             val locale = Locale.forLanguageTag(tag)
