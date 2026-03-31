8449b22d659711b248bca8b0e5af681a71e9691d
John Neerdael <jneerdael@netskope.com>
2026-03-31 19:15:08 +0200

    Fix trailer device auth release compile

--- app/src/main/java/com/nexio/tv/data/trailer/helper/YouTubeDeviceCodeAuthService.kt
+++ app/src/main/java/com/nexio/tv/data/trailer/helper/YouTubeDeviceCodeAuthService.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 
 private const val TAG = "YouTubeDeviceAuth"
