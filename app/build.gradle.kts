d5ab5de01463be3315fa4a29fb13da87f56dd836
John Neerdael <jneerdael@netskope.com>
2026-04-11 22:26:38 +0200

    feat: add subtitle translation provider request builders

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -426,6 +426,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     testImplementation("io.mockk:mockk:1.13.12")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("org.json:json:20240303")
     testImplementation("org.robolectric:robolectric:4.13")
     testImplementation("androidx.media3:media3-test-utils:1.10.0-beta01")
     debugImplementation("androidx.compose.ui:ui-tooling")
