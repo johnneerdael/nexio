9991a57a2ac0113c66456c6bb5d0f40f6c4bc78a
John Neerdael <jneerdael@netskope.com>
2026-04-20 15:17:24 +0200

    build: bump app version to 0.54

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -306,8 +306,8 @@ android {
         applicationId = "com.nexio.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 71
-        versionName = "0.53"
+        versionCode = 72
+        versionName = "0.54"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "INTRODB_API_URL", "\"${resolveProperty(localProperties, localProperties, "TIDB_API_URL").ifBlank { localProperties.getProperty("INTRODB_API_URL", "") }}\"")
