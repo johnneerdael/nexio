fd318678897d84aed0db4190835fb4e86506f531
John Neerdael <jneerdael@netskope.com>
2026-04-25 11:14:11 +0200

    test(auth): cover stock reset teardown regressions

--- app/src/test/java/com/nexio/tv/core/auth/LocalAccountResetCoordinatorTest.kt
+++ app/src/test/java/com/nexio/tv/core/auth/LocalAccountResetCoordinatorTest.kt
@@ -4,6 +4,7 @@ import com.nexio.tv.core.profile.ProfileManager
 import com.nexio.tv.core.sync.AccountSettingsSyncService
 import com.nexio.tv.data.local.AddonPreferences
 import io.mockk.coVerify
+import io.mockk.coVerifyOrder
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
@@ -25,5 +26,25 @@ class LocalAccountResetCoordinatorTest {
         coVerify { profileManager.resetToSingleDefaultProfile() }
         coVerify { addonPreferences.resetToDefaultAddons() }
         coVerify { accountSettingsSyncService.resetLocalAccountConfigToDefaults() }
+    }
+
+    @Test
+    fun `resetToSignedOutStockState resets profiles addons then account config in order`() = runTest {
+        val profileManager = mockk<ProfileManager>(relaxed = true)
+        val addonPreferences = mockk<AddonPreferences>(relaxed = true)
+        val accountSettingsSyncService = mockk<AccountSettingsSyncService>(relaxed = true)
+        val coordinator = LocalAccountResetCoordinator(
+            profileManager = profileManager,
+            addonPreferences = addonPreferences,
+            accountSettingsSyncService = javax.inject.Provider { accountSettingsSyncService }
+        )
+
+        coordinator.resetToSignedOutStockState()
+
+        coVerifyOrder {
+            profileManager.resetToSingleDefaultProfile()
+            addonPreferences.resetToDefaultAddons()
+            accountSettingsSyncService.resetLocalAccountConfigToDefaults()
+        }
     }
 }
