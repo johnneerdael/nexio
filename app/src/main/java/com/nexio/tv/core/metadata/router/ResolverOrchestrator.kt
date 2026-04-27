b853c10b6868a7159d0c262d09077d2383d4834d
John Neerdael <jneerdael@netskope.com>
2026-04-27 22:20:22 +0200

    feat(trace): emit metadata.resolver_schedule from ResolverOrchestrator

--- app/src/main/java/com/nexio/tv/core/metadata/router/ResolverOrchestrator.kt
+++ app/src/main/java/com/nexio/tv/core/metadata/router/ResolverOrchestrator.kt
@@ -1,10 +1,21 @@
 package com.nexio.tv.core.metadata.router
 
+import com.nexio.tv.core.trace.NoopRuntimeTraceSink
+import com.nexio.tv.core.trace.TraceMetadataEvents
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ResolverOrchestrator @Inject constructor() {
+class ResolverOrchestrator @Inject constructor(
+    private val traceEvents: TraceMetadataEvents
+) {
+    constructor() : this(
+        TraceMetadataEvents(
+            sink = NoopRuntimeTraceSink,
+            sessionId = { null }
+        )
+    )
+
     fun schedule(depth: MetadataDepth): ResolverSchedule {
         val localResolvers = mutableSetOf(ResolverType.ADDON_DISPLAY)
         val networkResolvers = mutableSetOf<ResolverType>()
@@ -34,6 +45,18 @@ class ResolverOrchestrator @Inject constructor() {
                 networkResolvers += ResolverType.SKIP_SEGMENTS
             }
         }
+
+        val scheduledSet = localResolvers + networkResolvers
+        val allTypes = ResolverType.values().toSet()
+        val skippedMap = (allTypes - scheduledSet).associate { type ->
+            type.name to "depth ${depth.name} not requested"
+        }
+
+        traceEvents.emitResolverSchedule(
+            depth = depth.name,
+            scheduled = scheduledSet.map { it.name },
+            skipped = skippedMap
+        )
 
         return ResolverSchedule(
             depth = depth,
