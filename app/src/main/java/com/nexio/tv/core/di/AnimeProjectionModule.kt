dcbdc91e8f8831010eaa8af1a6626a11bc7f5652
John Neerdael <jneerdael@netskope.com>
2026-05-06 03:04:11 +0200

    refactor(anime): retire AnimeSeasonPresentationCache and dead enum entries

--- app/src/main/java/com/nexio/tv/core/di/AnimeProjectionModule.kt
+++ app/src/main/java/com/nexio/tv/core/di/AnimeProjectionModule.kt
@@ -2,12 +2,10 @@ package com.nexio.tv.core.di
 
 import com.nexio.tv.core.anime.projection.AnimeEpisodeCoordinateStore
 import com.nexio.tv.core.anime.projection.AnimeSeasonDetailRepository
-import com.nexio.tv.core.anime.projection.AnimeSeasonPresentationCache
 import com.nexio.tv.core.anime.projection.AnimeSeasonProjectionResolver
 import com.nexio.tv.core.anime.projection.DefaultAnimeSeasonDetailRepository
 import com.nexio.tv.core.anime.projection.DefaultAnimeSeasonProjectionResolver
 import com.nexio.tv.core.anime.projection.InMemoryAnimeEpisodeCoordinateStore
-import com.nexio.tv.core.anime.projection.InMemoryAnimeSeasonPresentationCache
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -21,9 +19,6 @@ abstract class AnimeProjectionModule {
 
     @Binds
     abstract fun bindStore(impl: InMemoryAnimeEpisodeCoordinateStore): AnimeEpisodeCoordinateStore
-
-    @Binds
-    abstract fun bindPresentationCache(impl: InMemoryAnimeSeasonPresentationCache): AnimeSeasonPresentationCache
 
     @Binds
     abstract fun bindDetailRepository(impl: DefaultAnimeSeasonDetailRepository): AnimeSeasonDetailRepository
