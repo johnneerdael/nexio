cda1e903e5e9a3b5ee843ad7fc6707710d038a1c
John Neerdael <jneerdael@netskope.com>
2026-05-11 23:42:09 +0200

    0.57

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -350,8 +350,8 @@ android {
         applicationId = "com.nexio.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 74
-        versionName = "0.56"
+        versionCode = 75
+        versionName = "0.57"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "INTRODB_API_URL", "\"${resolveProperty(localProperties, localProperties, "TIDB_API_URL").ifBlank { localProperties.getProperty("INTRODB_API_URL", "") }}\"")
