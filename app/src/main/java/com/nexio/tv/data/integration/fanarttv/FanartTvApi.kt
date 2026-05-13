3dd3af52927296231b75fbf40059a2eed42d569c
John Neerdael <jneerdael@netskope.com>
2026-05-13 04:37:22 +0200

    feat(fanarttv): add Retrofit interface for v3.2 movie and tv lookups

---
+++ app/src/main/java/com/nexio/tv/data/integration/fanarttv/FanartTvApi.kt
@@ -0,0 +1,20 @@
+package com.nexio.tv.data.integration.fanarttv
+
+import com.nexio.tv.core.artwork.fanarttv.dto.FanartTvDocument
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface FanartTvApi {
+    @GET("v3.2/movies/{tmdbId}")
+    suspend fun getMovie(
+        @Path("tmdbId") tmdbId: String,
+        @Query("api_key") apiKey: String
+    ): FanartTvDocument
+
+    @GET("v3.2/tv/{tvdbId}")
+    suspend fun getTv(
+        @Path("tvdbId") tvdbId: String,
+        @Query("api_key") apiKey: String
+    ): FanartTvDocument
+}
